668a7e94ee9e7ba43503a13151fc153201a7c0ad
mujgonmavlonova-webcoder <mujgonmavlonova@gmail.com>
2026-05-25 11:21:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ async def main():
     logging.info("Telegram Bot Long Polling rejimida ishga tushmoqda...")
     await dp.start_polling(bot)
 
-if name == "__main__":
+if __name__ == "__main__":
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
